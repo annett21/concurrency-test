a1d674acbcb2b83bf44dced843d912c56765d6d2
annett21 <annettarasevich@gmail.com>
2023-02-07 16:04:40 +0300

    added dummy static endpoint

--- filestorage/static_files/views.py
+++ filestorage/static_files/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from time import sleep
 
-# Create your views here.
+
+def get_dummy_static_file(request, file_name):
+    print(f"Uploading {file_name} started.")
+    sleep(1)
+    print(f"Uploading {file_name} finished.")
+    return HttpResponse(200)
