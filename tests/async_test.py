8611195822d3422334a9f63f6a079988d6058173
annett21 <annettarasevich@gmail.com>
2023-02-07 20:26:17 +0300

    added async test

---
+++ tests/async_test.py
@@ -0,0 +1,38 @@
+import aiohttp
+import httpx
+import asyncio
+import time
+from utils import get_static_url
+
+
+static_url = get_static_url()
+
+async def get_file(url):
+    async with aiohttp.ClientSession(trust_env=True) as session:
+        async with session.get(url) as resp:
+            assert resp.status == 200
+
+
+async def get_file_httpx(url):
+    async with httpx.AsyncClient() as client:
+        print(url)
+        resp = await client.get(url)
+        assert resp.status_code == 200
+
+
+async def main():
+    tasks = []
+    for url in (static_url + str(filename) for filename in range(1, 16)):
+        tasks.append(loop.create_task(get_file_httpx(url)))
+    await asyncio.wait(tasks)
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
+    except Exception as e:
+        print(str(e))
+    end_time = time.time()
+    print(end_time - start_time)
