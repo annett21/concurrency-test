2508ad3ed1a2d294a32ebcb3fc79244e83102be9
annett21 <annettarasevich@gmail.com>
2023-02-07 21:12:15 +0300

    used ayncio.gather

--- tests/async_test.py
+++ tests/async_test.py
@@ -9,24 +9,20 @@ static_url = get_static_url()
 
 async def get_file(url):
     async with httpx.AsyncClient() as client:
-        print(url)
+        print(f"Uploading {url}: starting")
         resp = await client.get(url)
         assert resp.status_code == 200
+        print(f"Uploading {url}: finishing")
 
 
 async def main():
-    tasks = []
-    for url in (static_url + str(filename) for filename in range(1, 16)):
-        tasks.append(loop.create_task(get_file(url)))
-    await asyncio.wait(tasks)
+    urls = (static_url + str(name) for name in range(1, 16))
+    tasks = [asyncio.create_task(get_file(url)) for url in urls]
+    await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
     start_time = time.time()
-    try:
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main())
-    except Exception as e:
-        print(str(e))
+    asyncio.run(main())
     end_time = time.time()
     print(end_time - start_time)
